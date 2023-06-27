e974844b521aaa26d0c72541a042086abea3e922
Татьяна <ltasya72@gmail.com>
2023-06-27 19:29:31 +0300

    адаптив header, menu

--- app/js/main.js
+++ app/js/main.js
@@ -2,13 +2,19 @@ const headerBtn = document.querySelector(".header__btn");
 const rightsideMenu = document.querySelector(".rightside-menu");
 const rightsideMenuClose = document.querySelector(".rightside-menu__close");
 
+
 headerBtn.addEventListener("click", () => {
     rightsideMenu.classList.remove("rightside-menu--close");
 });
-
 rightsideMenuClose.addEventListener("click", () => {
     rightsideMenu.classList.add("rightside-menu--close");
 });
+
+$('.header__btn-menu').on('click', function () {
+    $('.menu').toggleClass('menu--open');
+}) 
+
+
 
 $(function () {
     $(".top__slider").slick({

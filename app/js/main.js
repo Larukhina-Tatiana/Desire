352e6a6c09d3ddf01df75a9a756c3946f4fcf5d9
Татьяна <ltasya72@gmail.com>
2023-07-01 21:43:18 +0300

    contacts-slider__list

--- app/js/main.js
+++ app/js/main.js
@@ -15,41 +15,64 @@ $('.header__btn-menu').on('click', function () {
 });
 $('.header__btn-menu').on('click', function () {
     $('.header__inner').toggleClass('header__inner-btn--open');
-}); 
+});
 
 
-if ($(window).width() < 641) {
-    $('.works-path__item--measurements').appendTo($('.works-path__items-box'));
-}
+// if ($(window).width() < 641) {
+//     $('.works-path__item--measurements').appendTo($('.works-path__items-box'));
+// }
 
 
 
 $(function () {
-    $(".top__slider").slick({
-        arrows: false,
-        dots: true,
-        fade: true,
-        autoplay: true,
-    });
+    // $(".top__slider").slick({
+    //     arrows: false,
+    //     dots: true,
+    //     fade: true,
+    //     autoplay: true,
+    // });
 
     $(".contacts-slider__list").slick({
-        slidesToShow: 10,
-        slidesToScroll: 10,
+        slidesToShow: 5,
+        slidesToScroll: 5,
         arrows: false,
         dots: true,
-        // autoplay: true,
+        responsive: [
+            {
+                breakpoint: 1781,
+                settings: {
+                    slidesToShow: 8,
+                    slidesToScroll: 8,
+                    dots: true
+                }
+            },
+            {
+                breakpoint: 1511,
+                settings: {
+                    slidesToShow: 6,
+                    slidesToScroll: 6
+                }
+            },
+            {
+                breakpoint: 1181,
+                settings: {
+                    slidesToShow: 4,
+                    slidesToScroll: 4
+                }
+            }
+        ]
     });
 
-    $(".article-slider__box").slick({
-        prevArrow: '<button type="button" class="article-slider__arrow article-slider__arrow--left"><img src="../images/prev.svg" alt="icon prev"></button>',
-        nextArrow: '<button type="button" class="article-slider__arrow article-slider__arrow--right"><img src="../images/next.svg" alt="icon next"></button>'
+$(".article-slider__box").slick({
+    prevArrow: '<button type="button" class="article-slider__arrow article-slider__arrow--left"><img src="../images/prev.svg" alt="icon prev"></button>',
+    nextArrow: '<button type="button" class="article-slider__arrow article-slider__arrow--right"><img src="../images/next.svg" alt="icon next"></button>'
 
-    });
+});
 
-    var mixer = mixitup(".gallery__inner", {
-        load: {
-            filter: ".office",
-        },
-    });
+var mixer = mixitup(".gallery__inner", {
+    load: {
+        filter: ".office",
+    },
+});
 
 });
